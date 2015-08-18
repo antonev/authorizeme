2be0ea76c069eccc7431d37c7209d67c166a6783
Anton Evdokimov <antonevv@gmail.com>
2015-08-18 17:26:00 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -11,18 +11,22 @@ setup(
     license='MIT',
     description='Authorization library for Python',
     author='Anton Evdokimov',
-    author_email='antonevv@gmail.co,',
+    author_email='antonevv@gmail.com',
     long_description='Authorization library for Python that can be used '
                      'with or without any Python web framework',
     py_modules=['authorizeme'],
     install_requires=[],
     zip_safe=False,
+    platforms='any',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
     ],
 )
